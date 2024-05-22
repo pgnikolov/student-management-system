1c24cc1b741300ae892e7505d7fd386354f948b0
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-22 21:55:31 +0200

    Added function to get data and function to add new student

--- students.py
+++ students.py
@@ -1,4 +1,21 @@
-def add_student(name, age, grade, subjects):
+import requests
+
+
+def get_studensts_data():
+    """
+    Get the students recordet data from Google Sheets with sheety.co API
+    Args:
+        No args
+    """
+    url = 'https://api.sheety.co/283e4374599ecb7c462c0903b64b4b25/students/students'
+    response = requests.get(url)
+    data = response.json()
+    students_data = data["students"]
+
+    return students_data
+
+
+def add_student(new_student: dict):
     """
     Add a new student record.
     Args:
@@ -7,7 +24,29 @@ def add_student(name, age, grade, subjects):
     - grade (float): The grade of the student.
     - subjects (list of str): The subjects the student is enrolled in.
     """
-    # Code to add a new student record
+    students_data = get_studensts_data()
+
+    # Check for existing student with the same name (assuming name is unique)
+    for student in students_data:
+        if student['name'] == new_student['name']:
+            print(f"Student {student['name']} already exists")
+            return  # Exit function if duplicate found
+    # Find the next free row id show number of the row in response
+    next_id = 1
+    if students_data:
+        next_id = max(student['id'] for student in students_data) + 1
+
+    # Create a new dictionary with the next ID
+    data_to_send = {"student": new_student}
+
+    url = 'https://api.sheety.co/283e4374599ecb7c462c0903b64b4b25/students/students'
+    response = requests.post(url, json=data_to_send)
+
+    # Check for successful addition
+    if response.status_code == 200:
+        print("Student added successfully!")
+    else:
+        print(f"Error adding student: {response.text}")
 
 
 def update_student(name):
@@ -47,4 +86,3 @@ def list_all_students():
     """
     # Check if there are any student records
     # Code to list all students
-
