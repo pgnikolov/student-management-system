ffdaf9c7b84f0d1108039e2ca817128f2d22338b
plamen <pgnikolov@gmail.com>
2024-06-11 19:51:05 +0200

    change

--- file_manage.py
+++ file_manage.py
@@ -1,5 +1,28 @@
 from openpyxl import Workbook, load_workbook
+from openpyxl.worksheet.worksheet import Worksheet
 
+
+def delete_row(sheet: Worksheet, first_name_del: str, last_name_del: str):
+    """
+        Delete all rows from a worksheet in the given workbook.
+    Args:
+        sheet: The worksheet to delete from.
+        first_name_del: Student's first name to delete.
+        last_name_del: Student's last name to delete.
+    Return:
+        sheet without the deleted row.
+    """
+
+    for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=2):
+        first_name = row[0].value
+        last_name = row[1].value
+
+        if first_name == first_name_del and last_name == last_name_del:
+            # Delete the row if the first and last names match
+            sheet.delete_rows(row[0].row)
+            break  # Exit the loop after deleting the first matching row
+
+    return sheet
 
 def new_all_students_custom(sheet_name: str, file_name: str, columns: int):
     """
@@ -59,7 +82,6 @@ def load_existing_file(file_path: str):
     """
     workbook = load_workbook(file_path)
 
-    print(workbook.sheetnames)
     return workbook
 
 
@@ -77,46 +99,3 @@ def save_existing_file(workbook, file_name: str):
     workbook.save(filename=file_name)
     print(f"Workbook saved as {file_name}")
 
-
-def file_menu():
-    """
-    Main function to provide user interaction.
-    """
-    current_workbook = None
-    available_sheets = None
-    while True:
-        # Display menu options
-        print("1. Create new custom file")
-        print("2. Create new default file")
-        print("3. Load existing file")
-        print("4. Save changes to an existing file")
-        print("5. Exit")
-
-        choice = input("Enter your choice: ")
-
-        if choice == '1':
-            sheetname = input("Enter a name for your sheet: ")
-            filename = input("Enter a name for your file: ")
-            amount_columns = int(input("How many columns do you want?: "))
-            new_all_students_custom(sheetname, filename, amount_columns)
-        elif choice == '2':
-            filename = input("Enter name for your file: ")
-            sheetname = input("Enter a name for your sheet: ")
-            new_all_students_default(filename, sheetname)
-        elif choice == '3':
-            file_path = input("Enter a file path: ")
-            result = load_existing_file(file_path)
-            current_workbook = result['workbook']
-            available_sheets = result['available_sheets']
-            print(f"Workbook loaded with sheets: {', '.join(available_sheets.keys())}")
-        elif choice == '4':
-            if current_workbook is None:
-                print("No workbook loaded. Please first load a workbook")
-            else:
-                file_name = input("Enter a file name with extension( .xlsx): ")
-                save_existing_file(current_workbook, file_name)
-        elif choice == '5':
-            print("Exiting...")
-            break
-        else:
-            print("Invalid choice. Please enter a number from 1 to 5.")
